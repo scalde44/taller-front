2f83a56ac3873bd853f12b3016246ff0f63daa38
scalde44 <scalderon1504@gmail.com>
2021-08-05 12:30:41 -0500

    Se agrega modulo mantenimiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,33 @@
+import { DatePipe } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from '@home/home.component';
-import { ProductoModule } from '@producto/producto.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@core/core.module';
+import { HomeComponent } from '@home/home.component';
 import { CookieService } from 'ngx-cookie-service';
-
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MantenimientoModule } from './feature/mantenimiento/mantenimiento.module';
+import { TrmComponent } from './feature/trm/trm.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TrmComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductoModule,
-    CoreModule
+    MantenimientoModule,
+    CoreModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
-  providers: [CookieService],
-    bootstrap: [AppComponent],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  providers: [CookieService, DatePipe],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
