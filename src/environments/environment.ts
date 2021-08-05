2de6d4a5c43742b7b98ca380b9a4b64f9b2f9085
scalde44 <scalderon1504@gmail.com>
2021-08-05 12:29:17 -0500

    Variables de entorno actualizadas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  endpoint: '/prospectos-vida'
+  endpoint: '/taller',
+  urlTrm: 'https://www.datos.gov.co/resource/ceyp-9c7c.json',
+  tokenTrm: 'JjZ6JrMR9SKo5CApGp2PX9KzJ'
 };
 
 /*
