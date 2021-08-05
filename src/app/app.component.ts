50e649068a7d4d7c669771c6ef2f7a606ac5191f
scalde44 <scalderon1504@gmail.com>
2021-08-05 12:31:01 -0500

    Pantalla inicial modificacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MenuItem } from '@core/modelo/menu-item';
 
 
 @Component({
@@ -9,11 +8,6 @@ import { MenuItem } from '@core/modelo/menu-item';
 })
 export class AppComponent {
   title = 'app-base';
-  public companies: MenuItem[] = [
-    { url: '/home', nombre: 'home' },
-    { url: '/producto', nombre: 'producto' }
-
-  ];
 
 
 }
