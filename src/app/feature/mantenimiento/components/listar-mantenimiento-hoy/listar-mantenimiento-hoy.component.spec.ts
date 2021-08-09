4c21618878dbc6833605efe8c0cfe88999fbd55b
scalde44 <scalderon1504@gmail.com>
2021-08-09 14:31:53 -0500

    Pruebas unitarias de lista de mantenimientos

--- src/app/feature/mantenimiento/components/listar-mantenimiento-hoy/listar-mantenimiento-hoy.component.spec.ts
+++ src/app/feature/mantenimiento/components/listar-mantenimiento-hoy/listar-mantenimiento-hoy.component.spec.ts
@@ -1,25 +1,59 @@
+import { CommonModule, DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpService } from '@core/services/http.service';
+import { of } from 'rxjs';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { Mantenimiento } from '../../shared/model/mantenimiento.model';
+import { MantenimientoService } from '../../shared/service/mantenimiento.service';
 import { ListarMantenimientoHoyComponent } from './listar-mantenimiento-hoy.component';
+
 
 describe('ListarMantenimientoHoyComponent', () => {
   let component: ListarMantenimientoHoyComponent;
   let fixture: ComponentFixture<ListarMantenimientoHoyComponent>;
+  let mantenimientoService: MantenimientoService;
+  const listadoMantenimientos: Mantenimiento[] = [
+    new Mantenimiento(1, 'ABC000', 250, '2021-08-05 12:00:00', 100000, 'A'),
+    new Mantenimiento(2, 'ABC001', 100, '2021-08-05 12:00:00', 80000, 'A'),
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListarMantenimientoHoyComponent ]
+      declarations: [ListarMantenimientoHoyComponent],
+      imports: [
+        CommonModule,
+        HttpClientModule,
+        AppRoutingModule
+      ],
+      providers: [MantenimientoService, HttpService, DatePipe]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarMantenimientoHoyComponent);
     component = fixture.componentInstance;
+    mantenimientoService = TestBed.inject(MantenimientoService);
+    spyOn(mantenimientoService, 'listarActivosPorFecha').and.returnValue(
+      of(listadoMantenimientos)
+    );
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`Comprobar que el titulo sea 'Listado de mantenimientos activos hoy'`, () => {
+    expect(component.titulo).toEqual('Listado de mantenimientos activos hoy');
+  });
+
+  it('Comprobar tamano lista de mantenimientos y que esten activos', () => {
+    component.listarMantenimientosActivosHoy();
+    expect(mantenimientoService.listarActivosPorFecha).toHaveBeenCalled();
+    expect(2).toBe(component.listaMantenimientosHoy.length);
+    expect('A').toBe(component.listaMantenimientosHoy[0].estado);
+    expect('A').toBe(component.listaMantenimientosHoy[1].estado);
   });
 });
--- src/app/feature/mantenimiento/components/listar-mantenimiento/listar-mantenimiento.component.spec.ts
+++ src/app/feature/mantenimiento/components/listar-mantenimiento/listar-mantenimiento.component.spec.ts
@@ -1,25 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpService } from '@core/services/http.service';
+import { of } from 'rxjs';
+import { Mantenimiento } from '../../shared/model/mantenimiento.model';
+import { MantenimientoService } from '../../shared/service/mantenimiento.service';
 import { ListarMantenimientoComponent } from './listar-mantenimiento.component';
+
 
 describe('ListarMantenimientoComponent', () => {
   let component: ListarMantenimientoComponent;
   let fixture: ComponentFixture<ListarMantenimientoComponent>;
-
+  let mantenimientoService: MantenimientoService;
+  const listadoMantenimientos: Mantenimiento[] = [
+    new Mantenimiento(1, 'ABC000', 250, '2021-08-05 12:00:00', 100000, 'A'),
+    new Mantenimiento(2, 'ABC001', 100, '2021-09-05 12:00:00', 80000, 'I'),
+  ];
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListarMantenimientoComponent ]
+      declarations: [ListarMantenimientoComponent],
+      imports: [
+        CommonModule,
+        HttpClientModule,
+        RouterTestingModule
+      ],
+      providers: [MantenimientoService, HttpService]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarMantenimientoComponent);
     component = fixture.componentInstance;
+    mantenimientoService = TestBed.inject(MantenimientoService);
+    spyOn(mantenimientoService, 'listar').and.returnValue(
+      of(listadoMantenimientos)
+    );
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`Comprobar que el titulo sea 'Listado de mantenimientos'`, () => {
+    expect(component.titulo).toEqual('Listado de mantenimientos');
+  });
+
+  it('Comprobar tamano lista de mantenimientos', () => {
+    component.listarMantenimientos();
+    expect(mantenimientoService.listar).toHaveBeenCalled();
+    expect(2).toBe(component.listaMantenimientos.length);
   });
 });
