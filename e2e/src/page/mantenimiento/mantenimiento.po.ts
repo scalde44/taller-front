de18108e74d69f94fbfe578c41976079c3aa062d
scalde44 <scalderon1504@gmail.com>
2021-08-10 16:10:18 -0500

    Se agregan nuevas funciones a mantenimiento page

--- e2e/src/page/mantenimiento/mantenimiento.po.ts
+++ e2e/src/page/mantenimiento/mantenimiento.po.ts
@@ -1,18 +1,49 @@
-import { by, element } from "protractor";
+import { by, element } from 'protractor';
 
 export class MantenimientoPage {
     private linkListarMantenimientos = element(by.id('linkListarMantenimientos'));
-    private tituloListaMantenimientos = element(by.id('tituloListaMantenimientos'));
+    private linkListarMantenimientosHoy = element(by.id('linkListarMantenimientosHoy'));
+    private linkCrearMantenimiento = element(by.id('linkCrearMantenimiento'));
     private listaMantenimientos = element.all(by.css('app-listar-mantenimiento tr.mat-row'));
-    async clickBotonListarMantenimientos() {
+    private listaMantenimientosHoy = element.all(by.css('app-listar-mantenimiento-hoy tr.mat-row'));
+    private inputPlacaMoto = element(by.id('placaMoto'));
+    private inputCilindrajeMoto = element(by.id('cilindrajeMoto'));
+    private inputFechaMoto = element(by.id('fechaMoto'));
+    private buttonCrearMantenimiento = element(by.id('btnCrearMoto'));
+
+    async clickLinkListarMantenimientos() {
         await this.linkListarMantenimientos.click();
     }
 
-    getTitleText() {
-        return this.tituloListaMantenimientos.getText() as Promise<string>;
+    async clickLinkListarMantenimientosHoy() {
+        await this.linkListarMantenimientosHoy.click();
     }
 
-    async contarMantenimientos() {
+    async clickLinkCrearMantenimiento() {
+        await this.linkCrearMantenimiento.click();
+    }
+
+    async clickBotonCrearMantenimiento() {
+        await this.buttonCrearMantenimiento.click();
+    }
+
+    async ingresarPlaca(placaMoto) {
+        await this.inputPlacaMoto.sendKeys(placaMoto);
+    }
+
+    async ingresarCilindraje(cilindrajeMoto) {
+        await this.inputCilindrajeMoto.sendKeys(cilindrajeMoto);
+    }
+
+    async ingresarFecha(fechaMoto) {
+        await this.inputFechaMoto.sendKeys(fechaMoto);
+    }
+
+    async contarMantenimientosTotales() {
         return await this.listaMantenimientos.count();
+    }
+
+    async contarMantenimientosHoy() {
+        return await this.listaMantenimientosHoy.count();
     }
 }
