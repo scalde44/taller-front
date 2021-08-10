f42918194d1224d60b7d938a65325c14a61ce796
scalde44 <scalderon1504@gmail.com>
2021-08-10 16:10:49 -0500

    Se agregan test e2e para mantenimiento page

--- e2e/src/test/mantenimiento.e2e-spec.ts
+++ e2e/src/test/mantenimiento.e2e-spec.ts
@@ -1,6 +1,6 @@
-import { AppPage } from "../app.po";
-import { MantenimientoPage } from "../page/mantenimiento/mantenimiento.po";
-import { NavbarPage } from "../page/navbar/navbar.po";
+import { AppPage } from '../app.po';
+import { MantenimientoPage } from '../page/mantenimiento/mantenimiento.po';
+import { NavbarPage } from '../page/navbar/navbar.po';
 
 describe('workspace-project Mantenimiento', () => {
     let page: AppPage;
@@ -13,11 +13,34 @@ describe('workspace-project Mantenimiento', () => {
         mantenimiento = new MantenimientoPage();
     });
 
-    it('Deberia listar mantenimientos', async() => {
+    it('Deberia listar todos los mantenimientos', async () => {
         await page.navigateTo();
         await navBar.clickBotonMantenimiento();
-        await mantenimiento.clickBotonListarMantenimientos();
+        await mantenimiento.clickLinkListarMantenimientos();
 
-        expect(mantenimiento.contarMantenimientos()).toBeGreaterThanOrEqual(0);
+        expect(mantenimiento.contarMantenimientosTotales()).toBeGreaterThanOrEqual(0);
+    });
+
+    it('Deberia listar los mantenimientos activos hoy', async () => {
+        await page.navigateTo();
+        await navBar.clickBotonMantenimiento();
+        await mantenimiento.clickLinkListarMantenimientosHoy();
+
+        expect(mantenimiento.contarMantenimientosHoy()).toBeGreaterThanOrEqual(0);
+    });
+
+    it('Deberia crear un mantenimiento', async () => {
+        const PLACA_MOTO = 'AKF23Z';
+        const CILINDRAJE_MOTO = '200';
+        const FECHA_MOTO = new Date().toLocaleString();
+
+        await page.navigateTo();
+        await navBar.clickBotonMantenimiento();
+        await mantenimiento.clickLinkCrearMantenimiento();
+
+        await mantenimiento.ingresarPlaca(PLACA_MOTO);
+        await mantenimiento.ingresarCilindraje(CILINDRAJE_MOTO);
+        await mantenimiento.ingresarFecha(FECHA_MOTO);
+        await mantenimiento.clickBotonCrearMantenimiento();
     });
 });
